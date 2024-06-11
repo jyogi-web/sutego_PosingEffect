d108c9b38b4dad0235109f450dca37b4fcd2653f
RiTa-23 <rita.tifrt@gmail.com>
2024-06-11 18:51:41 +0900

    [modify]デバッグ用ランドマーク表示

--- Assets/rita/cap_server.py
+++ Assets/rita/cap_server.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import struct
 import time
+import mediapipe as mp
 
 #IPアドレス取得
 hostname = socket.gethostname()
@@ -13,23 +14,60 @@ print("local_ipを取得："+str(local_ip))
 HOST = local_ip
 PORT = 50007
 
+# MediaPipeのposeモジュールを初期化(debug)
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(
+    static_image_mode=False,
+    model_complexity=1,
+    smooth_landmarks=True,
+    enable_segmentation=False,
+    smooth_segmentation=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+# 描画用のユーティリティを初期化(debug)
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
 # カメラのキャプチャ
 cap = cv2.VideoCapture(0)
 
 # TCP/IPソケットを作成し、サーバーに接続
+print("TCP/IPソケットを作成しています")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
+print("ソケット作成完了 -> クライアントからの接続を待ち受け中...")
 s.listen(1)
 conn, addr = s.accept()
+print("接続成功！")
 
 print('Connected by', addr)
 
 try:
+    print("キャプチャした画像を転送中...")
     while cap.isOpened():
         success, frame = cap.read()
         if not success:
             break
 
+        # BGR画像をRGBに変換(debug)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # 結果の描画をスキップするために画像を書き込み不可に設定(debug)
+        frame.flags.writeable = False
+        # MediaPipe Poseの処理を実行(debug)
+        results = pose.process(frame)
+        # 画像を書き込み可能に戻す(debug)
+        frame.flags.writeable = True
+        # RGB画像をBGRに変換(debug)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        # ポーズランドマークの描画(debug)
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        
         # 画像をバイト配列に変換
         _, buffer = cv2.imencode('.jpg', frame)
         byte_data = buffer.tobytes()
