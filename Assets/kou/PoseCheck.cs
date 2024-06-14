737c09b59bcfd3294d1a70a7f8a278a3495128d2
KOU050223 <kouhei20050223@icloud.com>
2024-06-14 20:22:09 +0900

    [add]グリコ・現場猫ポーズ判定追加

--- Assets/kou/PoseCheck.cs
+++ Assets/kou/PoseCheck.cs
@@ -28,7 +28,7 @@ public class PoseCheck : MonoBehaviour
     void Start()
     {
         Pos = PoseReceiver_kou.landmarkPosition;
-        currentState = PoseType.Lisp;  // 初期ステートを設定
+        currentState = PoseType.None;  // 初期ステートを設定
         UpdatePoseImage();
     }
 
@@ -38,21 +38,32 @@ public class PoseCheck : MonoBehaviour
         Pos = PoseReceiver_kou.landmarkPosition;  // 毎フレーム位置を更新
         
         // ここでポーズの位置に応じたステート変更を行う
-        //Listポーズ
+        //0:Lisp
         if (Math.Abs(Pos[0].y - Pos[13].y) <= 0.1 || Math.Abs(Pos[0].x - Pos[13].x) <= 0.1)
         {
             ChangeState(PoseType.Lisp);
         }
-        //きのきの
+        //1:きのきの
         else if (Pos[0].y < Pos[21].y && Pos[0].y < Pos[22].y)
         {
             ChangeState(PoseType.Kinokino);
         }
-        //ジョジョ
+        //2:波動拳
         else if (Pos[12].x > Pos[11].x)
         {
-            ChangeState(PoseType.Jojo);
+            ChangeState(PoseType.Hadouken);
         }//この下にあたらしいぽーずを追加
+        //4:現場猫
+        //右膝を挙げている・右手を横に向けている・左腕は横
+        else if (Pos[26].y > Pos[25].y && Math.Abs(Pos[12].z - Pos[16].z) <= 0.2 && Math.Abs(Pos[11].z - Pos[15].z) <= 0.2)
+        {
+            ChangeState(PoseType.Genbaneko);
+        }//5:グリコ
+        //左膝が上がっている・右手左手が上がっている
+        else if (Pos[26].y > Pos[25].y && Pos[16].y > Pos[12].y && Pos[15].y > Pos[11].y)
+        {
+            ChangeState(PoseType.Glico);
+        }
         else
         {
             ChangeState(PoseType.None);
@@ -75,6 +86,7 @@ public class PoseCheck : MonoBehaviour
             UpdatePoseImage();
             // ステート変更時の処理をここに追加
             checktext.text = newState.ToString();
+
         }
     }
 
